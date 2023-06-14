f2a5278288f88be3b0c59f32462e836a8efb6c32
hironow <hironow365@gmail.com>
2023-06-14 23:28:27 +0900

    modify settings

--- afpbb/settings.py
+++ afpbb/settings.py
@@ -20,12 +20,12 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 4
+# CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5 * 3  # robots.txt delay is 5 seconds and x3 for safety
+# DOWNLOAD_DELAY = 5  # delay by robots.txt
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
