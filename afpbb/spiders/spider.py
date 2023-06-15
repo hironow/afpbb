0a838d1a229ee682369ef7a0608762969432eee2
hironow <hironow365@gmail.com>
2023-06-15 22:20:25 +0900

    update for unknown error

--- afpbb/spiders/spider.py
+++ afpbb/spiders/spider.py
@@ -8,21 +8,30 @@ from scrapy.exceptions import CloseSpider
 class AfpbbSpider(scrapy.Spider):
     name = "afpbb"
     allowed_domains = ["www.afpbb.com"]
-    start_urls = ["https://www.afpbb.com/articles/-/3468222"]
+    start_urls = ["https://www.afpbb.com/articles/-/3300000"]
+
+    # causes an unknown error
+    skip_numbers = [
+        3307865,  # 502 status
+    ]
 
     # oldest accessible article number at 2023-06-14
-    oldest_number = 3300000
+    oldest_number = 3200000
 
-    handle_httpstatus_list = [404, 301]
+    handle_httpstatus_list = [404, 301, 502]
 
     def parse(self, response):
         self.logger.info(f"url: {response.url}")
         number = int(response.url.split("/")[-1])
 
+        # stop crawling when 502 error occurs and skipping the number
+        if response.status == 502 and number not in self.skip_numbers:
+            raise CloseSpider("502 error, stopping crawling.")
+
         title = response.css("h1::text").get()
         self.logger.info(f"number: {number}, title: {title}")
 
-        text = response.css("div.article-body").get() 
+        text = response.css("div.article-body").get()
         if response.status == 200 and title is not "記事はありません" and text is not None:
             # save html only when the file does not exist
             filename = f"html/{number}-article-from-afpbb.html"
@@ -36,10 +45,12 @@ class AfpbbSpider(scrapy.Spider):
                 ).strip()
                 yield AfpbbItem(text=plain_text, url=response.url)
 
-        if number >= self.oldest_number:
+        if number > self.oldest_number:
             # go to the next page (decrement the number)
             next_page = f"https://www.afpbb.com/articles/-/{number - 1}"
-            yield scrapy.Request(url=next_page, callback=self.parse, meta={'dont_redirect': True})
+            yield scrapy.Request(
+                url=next_page, callback=self.parse, meta={"dont_redirect": True}
+            )
         else:
             # stop crawling when the oldest article is reached
             # see: https://docs.scrapy.org/en/latest/topics/exceptions.html?highlight=closeSpider
